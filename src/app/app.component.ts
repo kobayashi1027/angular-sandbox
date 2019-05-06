f481de79cbf361f087a398e2ff133a84358b8abb
Hiroaki Kobayashi <koba1027yashi@gmail.com>
2019-05-07 05:43:49 +0900

    Add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-sandbox';
+  title = 'Angularサンプルページ';
+
+  public onClick(event: any) {
+    alert('ボタンがクリックされました');
+  }
 }
